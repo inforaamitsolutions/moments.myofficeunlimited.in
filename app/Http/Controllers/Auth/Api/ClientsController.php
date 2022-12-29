65db05482ece1346b468483c31159894057251cf
Bharat Chodvadiya <bharat.chodvadiya@codeclinic.in>
2022-12-29 17:11:36 +0530

    Client c capital in Clientscontroller

--- app/Http/Controllers/Auth/Api/ClientsController.php
+++ app/Http/Controllers/Auth/Api/ClientsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\auth\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use App\Models\client;
+use App\Models\Client;
 use App\Models\Loginuser;
 use App\Http\Requests\ClientsAddRequest;
 use Illuminate\Support\Facades\Validator;
